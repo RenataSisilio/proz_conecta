6f7dce0ab68a52b7128b6ad3fb7d60fa9592b514
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-01-14 19:39:53 -0300

    initialize assets and colors

---
+++ lib/design_sys/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart';
+
+class ProzColors {
+  static const purple = Color(0xff633197);
+  static const orange = Color(0xffff8000);
+  static const yellow = Color(0xfff0b316);
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff000000);
+  static const lightGrey = Color(0xffc7c7c7);
+  static const darkGrey = Color(0xff909090);
+}
