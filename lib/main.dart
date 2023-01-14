7e5608524d1a8d0edae7a0dd3639bd2c660742f3
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-01-14 20:37:33 -0300

    style: custom page transitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:proz_conecta/modules/onboarding/onboarding_page.dart';
 
 import 'design_sys/colors.dart';
 import 'modules/auth/initial_page_base.dart';
+import 'modules/onboarding/onboarding_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +14,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         visualDensity: VisualDensity.standard,
         primaryColor: ProzColors.purple,
         secondaryHeaderColor: ProzColors.orange,
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.windows: CupertinoPageTransitionsBuilder(),
+          },
+        ),
       ),
       initialRoute: '/',
       routes: {
